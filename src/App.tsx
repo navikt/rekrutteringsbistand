4ef2363df913dc157f15ca29b0cecee1e2441ea0
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2023-06-28 14:59:21 +0200

    Lag en mappe for forsiden (tidl. statistikk-appen)

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import useAmplitude from './header/useAmplitude';
 import Header from './header/Header';
+import Forside from './forside/Forside';
 
 const App = () => {
     const [navKontor, setNavKontor] = useState<string | null>(null);
@@ -13,22 +14,28 @@ const App = () => {
         {
             path: '/',
             element: <Header navKontor={navKontor} setNavKontor={setNavKontor} />,
-        },
-        {
-            path: 'stillinger/*',
-            element: <div>Placeholder</div>,
-        },
-        {
-            path: 'stillingssok/*',
-            element: <div>Placeholder</div>,
-        },
-        {
-            path: 'kandidater/*',
-            element: <div>Placeholder</div>,
-        },
-        {
-            path: 'kandidatsok/*',
-            element: <div>Placeholder</div>,
+            children: [
+                {
+                    path: '/',
+                    element: <Forside navKontor={navKontor} />,
+                },
+                {
+                    path: 'stillinger/*',
+                    element: <div>Placeholder</div>,
+                },
+                {
+                    path: 'stillingssok/*',
+                    element: <div>Placeholder</div>,
+                },
+                {
+                    path: 'kandidater/*',
+                    element: <div>Placeholder</div>,
+                },
+                {
+                    path: 'kandidatsok/*',
+                    element: <div>Placeholder</div>,
+                },
+            ],
         },
     ]);
 
---
+++ src/forside/Forside.tsx
@@ -0,0 +1,7 @@
+type Props = { navKontor: string | null };
+
+const Forside = ({ navKontor }: Props) => {
+    return <div>Forside</div>;
+};
+
+export default Forside;
