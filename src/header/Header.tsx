15ee5ce377b21f36b698c500f8937b8a919edaf9
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2023-06-29 14:51:41 +0200

    Importer mock til header i Header-komponenten selv

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -3,6 +3,10 @@ import Modiadekoratør from './modia/Modiadekoratør';
 import Navigeringsmeny from './navigeringsmeny/Navigeringsmeny';
 import useNavKontor from '../felles/store/navKontor';
 
+if (import.meta.env.VITE_MOCK) {
+    await import('./mock/mock-api');
+}
+
 const Header = () => {
     // useAmplitude(navKontor);
 
--- src/index.tsx
+++ src/index.tsx
@@ -6,14 +6,6 @@ import './index.css';
 const element = document.getElementById('rekrutteringsbistand');
 const root = createRoot(element as HTMLElement);
 
-const setupMock = async () => {
-    await import('./header/mock/mock-api');
-};
-
-if (import.meta.env.VITE_MOCK) {
-    setupMock();
-}
-
 root.render(
     <React.StrictMode>
         <App />
