821d334af4cf5c6014152dab39563457a6603028
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2023-07-03 10:43:04 +0200

    Skru av lazy-loading midlertidig

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -2,15 +2,15 @@ import { Outlet } from 'react-router-dom';
 import Modiadekoratør from './modia/Modiadekoratør';
 import Navigeringsmeny from './navigeringsmeny/Navigeringsmeny';
 import useNavKontor from '../felles/store/navKontor';
+import useAmplitude from './useAmplitude';
 
 if (import.meta.env.VITE_MOCK) {
     await import('./mock/mock-api');
 }
 
 const Header = () => {
-    // useAmplitude(navKontor);
-
     const { navKontor, setNavKontor } = useNavKontor();
+    useAmplitude(navKontor);
 
     return (
         <>
