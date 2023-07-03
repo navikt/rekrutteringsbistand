564bec3e90768a84d7b07ed4fc16e714d2e98239
Kjetil Svalestuen <kjesvale@users.noreply.github.com>
2023-07-03 10:59:33 +0200

    Én bundle (#4)
    * Skru av lazy-loading midlertidig
    
    * Enable events

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,33 @@ import {
 } from 'react-router-dom';
 import Header from './header/Header';
 
+import { Component as Forside } from './forside/index';
+import { Component as Stillingssøk } from './stillingssok/index';
+import { Component as Stilling } from './stilling/index';
+import { Component as Kandidatsøk } from './kandidatsok/index';
+import { Component as Kandidat } from './kandidat/index';
+
 const App = () => {
     const router = createBrowserRouter(
         createRoutesFromElements(
             <Route path="/" element={<Header />}>
-                <Route path="/" lazy={() => import('./forside/index')} />
-                <Route path="stillingssok/:fnr?" lazy={() => import('./stillingssok/index')} />
-                <Route path="stillinger/*" lazy={() => import('./stilling/index')} />
-                <Route path="kandidatsok" lazy={() => import('./kandidatsok/index')} />
-                <Route path="kandidater/*" lazy={() => import('./kandidat/index')} />
+                <Route path="/" element={<Forside />} /*lazy={() => import('./forside/index')}*/ />
+                <Route
+                    path="stillingssok/:fnr?"
+                    element={<Stillingssøk />} /*lazy={() => import('./stillingssok/index')}*/
+                />
+                <Route
+                    path="stillinger/*"
+                    element={<Stilling />} /*lazy={() => import('./stilling/index')}*/
+                />
+                <Route
+                    path="kandidatsok"
+                    element={<Kandidatsøk />} /*lazy={() => import('./kandidatsok/index')}*/
+                />
+                <Route
+                    path="kandidater/*"
+                    element={<Kandidat />} /*lazy={() => import('./kandidat/index')}*/
+                />
             </Route>
         )
     );
