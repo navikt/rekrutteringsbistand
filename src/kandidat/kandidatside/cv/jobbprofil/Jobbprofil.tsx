409056847520e7ee619d4f1d5dd696211624abba
vildestefferud <qwartz.ms@hotmail.com>
2023-07-19 15:59:03 +0200

    Oppdater design for jobbprofil

--- src/kandidat/kandidatside/cv/jobbprofil/Jobbprofil.tsx
+++ src/kandidat/kandidatside/cv/jobbprofil/Jobbprofil.tsx
@@ -1,74 +1,74 @@
-import { FunctionComponent } from 'react';
-import { BodyShort } from '@navikt/ds-react';
 import Cv from '../reducer/cv-typer';
-import Informasjonspanel from '../Informasjonspanel';
+import { BodyShort, Heading, Panel } from '@navikt/ds-react';
+import { HeartIcon, PinIcon, ClockIcon, TimerStartIcon, HourglassIcon } from '@navikt/aksel-icons';
 import css from './Jobbprofil.module.css';
 
 const oppstartskoder = {
-    LEDIG_NAA: { key: 'LEDIG_NAA', label: 'Kandidaten er ledig nå' },
-    ETTER_TRE_MND: { key: 'ETTER_TRE_MND', label: 'Kandidaten har 3 måneder oppsigelse' },
-    ETTER_AVTALE: { key: 'ETTER_AVTALE', label: 'Kandidaten er ledig etter avtale' },
+    LEDIG_NAA: { key: 'LEDIG_NAA', label: 'Nå' },
+    ETTER_TRE_MND: { key: 'ETTER_TRE_MND', label: '3 måneders oppsigelse' },
+    ETTER_AVTALE: { key: 'ETTER_AVTALE', label: 'Etter avtale' },
 };
 
 type Props = {
     cv: Cv;
 };
 
-const Jobbønsker: FunctionComponent<Props> = ({ cv }) => (
-    <Informasjonspanel tittel="Jobbprofil">
-        {cv.yrkeJobbonsker?.length > 0 && (
-            <dl className={css.liste}>
-                <Description
-                    label="Ønsket yrke"
-                    items={cv.yrkeJobbonsker.map((j) => j.styrkBeskrivelse)}
+const Jobbprofil = ({ cv }: Props) => {
+    return (
+        <Panel border className={css.panel}>
+            <Heading level="2" size="small" className={css.panelOverskrift}>
+                <div className={css.overskriftMedIkon}>
+                    <div className={css.ikonRamme}>
+                        <HeartIcon />
+                    </div>
+                    Ønsker
+                </div>
+            </Heading>
+            <BodyShort size="medium" className={css.jobbønsker}>
+                <MangeTekstelementerSeparertMedKomma
+                    elementer={cv.yrkeJobbonsker.map((j) => j.styrkBeskrivelse)}
                 />
-
-                <Description
-                    label="Kompetanse"
-                    items={cv.kompetanse.map((u) => u.kompetanseKodeTekst)}
+            </BodyShort>
+            <BodyShort size="small" className={css.kompetanse}>
+                <MangeTekstelementerSeparertMedKomma
+                    elementer={cv.kompetanse.map((u) => u.kompetanseKodeTekst)}
                 />
-
-                <Description
-                    label="Ønsket sted"
-                    items={cv.geografiJobbonsker.map((u) => u.geografiKodeTekst)}
-                />
-
-                <Description
-                    label="Heltid/deltid"
-                    items={cv.omfangJobbprofil.map((u) => u.heltidDeltidKodeTekst)}
-                />
-
-                <Description
-                    label="Arbeidstid"
-                    items={cv.arbeidstidJobbprofil.map((u) => u.arbeidstidKodeTekst)}
-                />
-
-                <Description
-                    label="Arbeidsforhold"
-                    items={cv.ansettelsesformJobbprofil.map((u) => u.ansettelsesformKodeTekst)}
-                />
-
-                {cv.oppstartKode && (
-                    <Description
-                        label="Ledighet"
-                        items={[oppstartskoder[cv.oppstartKode.toUpperCase()]?.label]}
+            </BodyShort>
+            <div className={css.bunn}>
+                <BodyShort size="small" className={css.uthevetTekstOgIkon}>
+                    <PinIcon />
+                    <MangeTekstelementerSeparertMedMellomrom
+                        elementer={cv.geografiJobbonsker.map((u) => u.geografiKodeTekst)}
                     />
-                )}
-            </dl>
-        )}
-    </Informasjonspanel>
-);
+                </BodyShort>
+                <BodyShort size="small" className={css.uthevetTekstOgIkon}>
+                    <ClockIcon />
+                    <MangeTekstelementerSeparertMedOg
+                        elementer={cv.omfangJobbprofil.map((u) => u.heltidDeltidKodeTekst)}
+                    />
+                </BodyShort>
+                <BodyShort size="small" className={css.uthevetTekstOgIkon}>
+                    <TimerStartIcon />
+                    <MangeTekstelementerSeparertMedOg
+                        elementer={cv.arbeidstidJobbprofil.map((u) => u.arbeidstidKodeTekst)}
+                    />
+                </BodyShort>
+                <BodyShort size="small" className={css.uthevetTekstOgIkon}>
+                    <HourglassIcon />
+                    <MangeTekstelementerSeparertMedKomma
+                        elementer={[oppstartskoder[cv.oppstartKode.toUpperCase()]?.label]}
+                    />
+                </BodyShort>
+            </div>
+        </Panel>
+    );
+};
 
-const Description = ({ label, items }: { label: string; items: Array<string | null> }) => (
-    <>
-        <BodyShort as="dt">{label}</BodyShort>
-        <BodyShort as="dd">
-            <MangeTekstelementer elementer={items} />
-        </BodyShort>
-    </>
-);
-
-const MangeTekstelementer = ({ elementer }: { elementer: Array<string | null> }) => {
+const MangeTekstelementerSeparertMedKomma = ({
+    elementer,
+}: {
+    elementer: Array<string | null>;
+}) => {
     return (
         <span>
             {elementer
@@ -79,4 +79,30 @@ const MangeTekstelementer = ({ elementer }: { elementer: Array<string | null> })
     );
 };
 
-export default Jobbønsker;
+const MangeTekstelementerSeparertMedOg = ({ elementer }: { elementer: Array<string | null> }) => {
+    return (
+        <span>
+            {elementer
+                .filter((e) => e !== null)
+                .map((element) => element)
+                .join(' og ')}
+        </span>
+    );
+};
+
+const MangeTekstelementerSeparertMedMellomrom = ({
+    elementer,
+}: {
+    elementer: Array<string | null>;
+}) => {
+    return (
+        <span>
+            {elementer
+                .filter((e) => e !== null)
+                .map((element) => element)
+                .join(' ')}
+        </span>
+    );
+};
+
+export default Jobbprofil;
