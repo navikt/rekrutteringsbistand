fa85ba994e1743667959c7194db78586e7bd4d24
vildestefferud <qwartz.ms@hotmail.com>
2023-07-27 11:30:18 +0200

    Lag kort

---
+++ src/kandidat/kandidatside/cvNy/erfaringer/erfaring/Erfaring.tsx
@@ -0,0 +1,26 @@
+import css from './Erfaring.module.css';
+import { BodyShort } from '@navikt/ds-react';
+import { ReactNode } from 'react';
+
+type Props = {
+    overskrift: string;
+    beskrivelse: string;
+    tidsperiode: ReactNode;
+};
+const Erfaring = ({ overskrift, beskrivelse, tidsperiode }: Props) => {
+    return (
+        <div className={css.erfaring}>
+            <div className={css.innhold}>
+                <BodyShort size="small" className={css.overskrift}>
+                    {overskrift}
+                </BodyShort>
+                {tidsperiode}
+                <BodyShort size="small" className={css.beskrivelse}>
+                    {beskrivelse}
+                </BodyShort>
+            </div>
+        </div>
+    );
+};
+
+export default Erfaring;
