9fb15c6e23919ff9d885f5e32f0c0165e2fd2700
frodank <11638682+frodank@users.noreply.github.com>
2023-08-01 14:21:29 +0200

    setter opp mock

--- mock/presenterte-kandidater-api/mock.ts
+++ mock/presenterte-kandidater-api/mock.ts
@@ -1,7 +1,7 @@
 import { rest } from 'msw';
 import { api } from '../../src/felles/api';
 
-export const kandidatApiMock = [
+export const presenterteKandidaterApiMock = [
     rest.get(
         `${api.presenterteKandidaterApi}/kandidatliste/3d2e6a60-9cd5-551a-b8cf-8fce1954c5da/vurdering`,
         (_, res, ctx) => res(ctx.json(mockArbeidsgiversVurderinger))
