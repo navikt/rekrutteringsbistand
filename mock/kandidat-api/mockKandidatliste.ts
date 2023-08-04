4d7be5a58205811f3bf371fc3af8f67188020368
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2023-08-04 08:58:47 +0200

    Lag enkle mocks for kandidat, kandidat i kandidatliste og kandidat i elastic

--- mock/kandidat-api/mockKandidatliste.ts
+++ mock/kandidat-api/mockKandidatliste.ts
@@ -1,17 +1,31 @@
-import Kandidatliste from 'felles/domene/kandidatliste/Kandidatliste';
+import Kandidatliste, {
+    KanSletteKandidatliste,
+    Kandidatlistestatus,
+} from 'felles/domene/kandidatliste/Kandidatliste';
+import { mockMeg } from '../meg/mock';
+import { mockKandidatIKandidatliste } from './mockKandidatIKandidatliste';
 
-export const mockKandidatliste: Kandidatliste = {
-    kandidatlisteId: '123',
-    stillingId: 'abc',
+export const kandidatlisteUtenStilling: Kandidatliste = {
+    kandidatlisteId: 'bf6877fa-5c82-4610-8cf7-ff7a0df18e29',
     tittel: 'Volleyballskuespiller på Pescara Beach',
-    organisasjonNavn: null,
-    antallKandidater: 1,
-    beskrivelse: null,
-    organisasjonReferanse: null,
+    beskrivelse: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
 
+    kanEditere: true,
+    kanSlette: KanSletteKandidatliste.KanSlettes,
     opprettetTidspunkt: new Date().toISOString(),
+    status: Kandidatlistestatus.Åpen,
+
+    stillingskategori: null,
+    organisasjonNavn: null,
+    organisasjonReferanse: null,
+    stillingId: null,
+    antallStillinger: null,
+
+    kandidater: mockKandidatIKandidatliste,
+    formidlingerAvUsynligKandidat: [],
+
     opprettetAv: {
-        ident: 'AB123456',
-        navn: 'Joare Mangstuen Mossby',
+        ident: mockMeg.navIdent,
+        navn: 'Varg Veileder',
     },
 };
---
+++ mock/kandidatsok-proxy/mockKandidat.ts
@@ -0,0 +1,101 @@
+import Kandidat from 'felles/domene/kandidat/Kandidat';
+import {
+    Formidlingsgruppe,
+    Hovedmål,
+    Innsatsgruppe,
+} from 'felles/domene/kandidat/Oppfølgingsinformasjon';
+import { mockMeg } from '../meg/mock';
+
+export const mockKandidat: Kandidat = {
+    // Personalia
+    fornavn: 'Jarle',
+    etternavn: 'Jobbsøker',
+    fodselsdato: '1945-11-14',
+    epostadresse: 'jarle.jobbsoker@dev.nav.no',
+    telefon: '91333532',
+
+    // Id
+    aktorId: '1000102960567',
+    fodselsnummer: '14114536327',
+    arenaKandidatnr: 'AB123456',
+    kandidatnr: 'AB123456',
+
+    // Geografi
+    kommunenummer: 301,
+    kommunenummerstring: '0301',
+    postnummer: '0662',
+    poststed: 'OSLO',
+    adresselinje1: 'Arbeids- og velferdsdirektoratet, Sannergata 2',
+
+    // Oppfølgingsinformasjon
+    navkontor: 'NAV Lofoten',
+    orgenhet: '1860',
+    hovedmaalkode: Hovedmål.SkaffeArbeid,
+    kvalifiseringsgruppekode: Innsatsgruppe.SituasjonsbestemtInnsats,
+    formidlingsgruppekode: Formidlingsgruppe.Arbeidssøker,
+    veileder: mockMeg.navIdent,
+
+    // Jobbprofil
+    yrkeJobbonskerObj: [
+        {
+            primaertJobbonske: true,
+            sokeTitler: [],
+            styrkBeskrivelse: 'Gartner',
+            styrkKode: '01',
+        },
+    ],
+    geografiJobbonsker: [
+        {
+            geografiKodeTekst: 'Geiranger',
+            geografiKode: '1000',
+        },
+    ],
+    arbeidsdagerJobbonskerObj: [],
+    oppstartKode: null,
+    arbeidstidsordningJobbonskerObj: [],
+    arbeidstidJobbonskerObj: [],
+    ansettelsesformJobbonskerObj: [],
+    omfangJobbonskerObj: [],
+
+    // Cv
+    beskrivelse:
+        'Som en dyktig urmaker med 12 års erfaring, har jeg opparbeidet meg omfattende kunnskap og ferdigheter innen reparasjon, vedlikehold og produksjon av ur. Jeg har erfaring med å håndtere en rekke forskjellige typer ur, fra mekaniske klokker til moderne smartklokker. I mitt tidligere arbeid som urmaker har jeg opparbeidet meg en god forståelse av kundens behov, og jeg er alltid forberedt på å yte den beste servicen og kvalitetsarbeidet for å sikre kundetilfredshet. Jeg er også vant til å arbeide effektivt og nøyaktig for å møte stramme tidsfrister.',
+    sprak: [],
+    yrkeserfaring: [],
+    utdanning: [],
+    forerkort: [],
+    fagdokumentasjon: [],
+    godkjenninger: [],
+    kursObj: [],
+    annenerfaringObj: [],
+    kompetanseObj: [],
+    samletKompetanseObj: [],
+    sertifikatObj: [],
+    vervObj: [],
+    perioderMedInaktivitet: [],
+
+    // Ubrukte felter
+    totalLengdeYrkeserfaring: 0,
+    samtykkeStatus: 'G',
+    samtykkeDato: '2019-01-29T15:19:28.361+00:00',
+    harKontaktinformasjon: true,
+    tidsstempel: new Date().toISOString(),
+    adresselinje2: '',
+    adresselinje3: '',
+    doed: false,
+    frKode: '0',
+    fylkeNavn: 'Nordland',
+    kommuneNavn: 'Vestvågøy',
+    kommunenummerkw: 301,
+    fodselsdatoErDnr: false,
+    synligForArbeidsgiverSok: true,
+    synligForVeilederSok: true,
+
+    // Utgåtte felter
+    mobiltelefon: null,
+    statsborgerskap: null,
+    disponererBil: null,
+    landkode: null,
+    fritattAgKandidatsok: null,
+    fritattKandidatsok: null,
+};
