2e67f048939567f677b5c5f89431d2ddfc10331b
vildestefferud <qwartz.ms@hotmail.com>
2023-08-17 10:25:04 +0200

    Endre feilmelding

--- src/kandidat/kandidatside/cv/CvSide.tsx
+++ src/kandidat/kandidatside/cv/CvSide.tsx
@@ -4,7 +4,6 @@ import { useSelector } from 'react-redux';
 import { Nettstatus } from 'felles/nettressurs';
 import AppState from '../../state/AppState';
 import IkkeFunnet from './ikke-funnet/IkkeFunnet';
-import Sidefeil from '../../komponenter/sidefeil/Sidefeil';
 import Sidelaster from '../../komponenter/sidelaster/Sidelaster';
 import css from './CvSide.module.css';
 import Jobbønsker from './jobbønsker/Jobbønsker';
@@ -17,6 +16,7 @@ import Godkjenninger from './cv/Godkjenninger';
 import Kurs from './cv/Kurs';
 import Sammendrag from './cv/Sammendrag';
 import Kompetanse from './cv/Kompetanse';
+import { Alert, BodyLong, Heading } from '@navikt/ds-react';
 
 const CvSide: FunctionComponent = () => {
     const { cv } = useSelector((state: AppState) => state.cv);
@@ -54,7 +54,22 @@ const CvSide: FunctionComponent = () => {
         );
     }
 
-    return <Sidefeil feilmelding="Klarte ikke å hente kandidatens CV" />;
+    return (
+        <div className={css.side}>
+            <div className={css.wrapper}>
+                <Alert variant="error">
+                    <Heading size="medium">Vi klarte ikke å hente detaljer om kandidaten</Heading>
+                    <BodyLong size="medium">
+                        Det kan være fordi kandidaten mangler informasjon på CVen, eller på grunn av
+                        feil i systemene våre. Sjekk om kandidaten har fyllt ut CVen sin i
+                        Modia-Arbeidsrettet Oppfølging. Etterpå kan du prøve å laste inn denne siden
+                        på nytt. Hvis feilen vedvarer kan du kontakte brukerstøtte til
+                        rekrutteringsbitand for å få hjelp med feilen.
+                    </BodyLong>
+                </Alert>
+            </div>
+        </div>
+    );
 };
 
 export default CvSide;
