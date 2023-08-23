f6d930e217bab551c79358f6abe03fb25a4669d1
Vilde Stefferud <33753494+vildestefferud@users.noreply.github.com>
2023-08-23 12:25:05 +0200

    Legge til tittel og fikse ikonene (#34)
    * Legge til tittel og fikse ikonene
    
    * Refaktorere kode
    
    * Fjerner egen aria-labels siden vi nå har labels

--- src/kandidat/kandidatside/cv/cv/jobbønsker/Jobbønsker.tsx
+++ src/kandidat/kandidatside/cv/cv/jobbønsker/Jobbønsker.tsx
@@ -1,13 +1,20 @@
 import { BodyShort } from '@navikt/ds-react';
-import { ClockIcon, HeartIcon, HourglassIcon, PinIcon, TimerStartIcon } from '@navikt/aksel-icons';
+import {
+    ClockIcon,
+    HeartIcon,
+    TimerStartIcon,
+    PinIcon,
+    BriefcaseClockIcon,
+} from '@navikt/aksel-icons';
 import css from './Jobbønsker.module.css';
 import Kort from '../kort/Kort';
 import { KandidatCv } from 'felles/domene/kandidat/Kandidat';
+import { ReactNode } from 'react';
 
 const oppstartskoder = {
-    LEDIG_NAA: { key: 'LEDIG_NAA', label: 'Ledig nå' },
-    ETTER_TRE_MND: { key: 'ETTER_TRE_MND', label: '3 måneders oppsigelsestid' },
-    ETTER_AVTALE: { key: 'ETTER_AVTALE', label: 'Oppstart etter avtale' },
+    LEDIG_NAA: { key: 'LEDIG_NAA', label: 'Nå' },
+    ETTER_TRE_MND: { key: 'ETTER_TRE_MND', label: 'Om 3 måneder (oppsigelsestid)' },
+    ETTER_AVTALE: { key: 'ETTER_AVTALE', label: 'Etter avtale' },
 };
 
 type Props = {
@@ -27,8 +34,7 @@ const Jobbønsker = ({ cv }: Props) => {
                 )}
             </BodyShort>
             <ul className={css.bunn}>
-                <BodyShort as="li" size="small" aria-label="Ønsket sted">
-                    <PinIcon aria-hidden />
+                <Jobbønskeinformasjon label="Sted" ikon={<PinIcon aria-hidden />}>
                     {cv.geografiJobbonsker?.length > 0 ? (
                         <MangeTekstelementerSeparertMedMellomrom
                             elementer={cv.geografiJobbonsker.map((u) => u.geografiKodeTekst)}
@@ -36,9 +42,11 @@ const Jobbønsker = ({ cv }: Props) => {
                     ) : (
                         '-'
                     )}
-                </BodyShort>
-                <BodyShort as="li" size="small" aria-label="Omfang">
-                    <ClockIcon aria-hidden />
+                </Jobbønskeinformasjon>
+                <Jobbønskeinformasjon
+                    label="Heltid/Deltid"
+                    ikon={<BriefcaseClockIcon aria-hidden />}
+                >
                     {cv.omfangJobbprofil?.length > 0 ? (
                         <MangeTekstelementerSeparertMedOg
                             elementer={cv.omfangJobbprofil.map((u) => u.heltidDeltidKodeTekst)}
@@ -46,9 +54,8 @@ const Jobbønsker = ({ cv }: Props) => {
                     ) : (
                         '-'
                     )}
-                </BodyShort>
-                <BodyShort as="li" size="small" aria-label="Arbeidstid">
-                    <TimerStartIcon aria-hidden />
+                </Jobbønskeinformasjon>
+                <Jobbønskeinformasjon label="Arbeidstid" ikon={<ClockIcon aria-hidden />}>
                     {cv.arbeidstidJobbprofil?.length > 0 ? (
                         <MangeTekstelementerSeparertMedOg
                             elementer={cv.arbeidstidJobbprofil.map((u) => u.arbeidstidKodeTekst)}
@@ -56,9 +63,8 @@ const Jobbønsker = ({ cv }: Props) => {
                     ) : (
                         '-'
                     )}
-                </BodyShort>
-                <BodyShort as="li" size="small" aria-label="Oppstart">
-                    <HourglassIcon aria-hidden />
+                </Jobbønskeinformasjon>
+                <Jobbønskeinformasjon label="Kan starte" ikon={<TimerStartIcon aria-hidden />}>
                     {cv.oppstartKode ? (
                         <MangeTekstelementerSeparertMedKomma
                             elementer={[oppstartskoder[cv.oppstartKode]?.label]}
@@ -66,11 +72,29 @@ const Jobbønsker = ({ cv }: Props) => {
                     ) : (
                         '-'
                     )}
-                </BodyShort>
+                </Jobbønskeinformasjon>
             </ul>
         </Kort>
     );
 };
+
+const Jobbønskeinformasjon = ({
+    label,
+    ikon,
+    children,
+}: {
+    label: string;
+    ikon: ReactNode;
+    children: ReactNode;
+}) => (
+    <BodyShort as="li" size="small">
+        {ikon}
+        <div>
+            <BodyShort size="small">{label}</BodyShort>
+            {children}
+        </div>
+    </BodyShort>
+);
 
 export const MangeTekstelementerSeparertMedKomma = ({
     elementer,
