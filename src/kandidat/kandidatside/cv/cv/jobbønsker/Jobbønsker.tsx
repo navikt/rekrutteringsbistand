b8763f5ee4987627f41418af6e678fb87b32baaf
vildestefferud <qwartz.ms@hotmail.com>
2023-08-23 11:24:39 +0200

    Legge til tittel og fikse ikonene

--- src/kandidat/kandidatside/cv/cv/jobbønsker/Jobbønsker.tsx
+++ src/kandidat/kandidatside/cv/cv/jobbønsker/Jobbønsker.tsx
@@ -1,13 +1,19 @@
 import { BodyShort } from '@navikt/ds-react';
-import { ClockIcon, HeartIcon, HourglassIcon, PinIcon, TimerStartIcon } from '@navikt/aksel-icons';
+import {
+    ClockIcon,
+    HeartIcon,
+    TimerStartIcon,
+    PinIcon,
+    BriefcaseClockIcon,
+} from '@navikt/aksel-icons';
 import css from './Jobbønsker.module.css';
 import Kort from '../kort/Kort';
 import { KandidatCv } from 'felles/domene/kandidat/Kandidat';
 
 const oppstartskoder = {
-    LEDIG_NAA: { key: 'LEDIG_NAA', label: 'Ledig nå' },
-    ETTER_TRE_MND: { key: 'ETTER_TRE_MND', label: '3 måneders oppsigelsestid' },
-    ETTER_AVTALE: { key: 'ETTER_AVTALE', label: 'Oppstart etter avtale' },
+    LEDIG_NAA: { key: 'LEDIG_NAA', label: 'Nå' },
+    ETTER_TRE_MND: { key: 'ETTER_TRE_MND', label: 'Om 3 måneder (oppsigelsestid)' },
+    ETTER_AVTALE: { key: 'ETTER_AVTALE', label: 'Etter avtale' },
 };
 
 type Props = {
@@ -29,43 +35,57 @@ const Jobbønsker = ({ cv }: Props) => {
             <ul className={css.bunn}>
                 <BodyShort as="li" size="small" aria-label="Ønsket sted">
                     <PinIcon aria-hidden />
-                    {cv.geografiJobbonsker?.length > 0 ? (
-                        <MangeTekstelementerSeparertMedMellomrom
-                            elementer={cv.geografiJobbonsker.map((u) => u.geografiKodeTekst)}
-                        />
-                    ) : (
-                        '-'
-                    )}
+                    <div>
+                        <BodyShort size="small">Sted</BodyShort>
+                        {cv.geografiJobbonsker?.length > 0 ? (
+                            <MangeTekstelementerSeparertMedMellomrom
+                                elementer={cv.geografiJobbonsker.map((u) => u.geografiKodeTekst)}
+                            />
+                        ) : (
+                            '-'
+                        )}
+                    </div>
                 </BodyShort>
-                <BodyShort as="li" size="small" aria-label="Omfang">
-                    <ClockIcon aria-hidden />
-                    {cv.omfangJobbprofil?.length > 0 ? (
-                        <MangeTekstelementerSeparertMedOg
-                            elementer={cv.omfangJobbprofil.map((u) => u.heltidDeltidKodeTekst)}
-                        />
-                    ) : (
-                        '-'
-                    )}
+                <BodyShort as="li" size="small" aria-label="Heltid/Deltid">
+                    <BriefcaseClockIcon aria-hidden />
+                    <div>
+                        <BodyShort size="small">Heltid/Deltid</BodyShort>
+                        {cv.omfangJobbprofil?.length > 0 ? (
+                            <MangeTekstelementerSeparertMedOg
+                                elementer={cv.omfangJobbprofil.map((u) => u.heltidDeltidKodeTekst)}
+                            />
+                        ) : (
+                            '-'
+                        )}
+                    </div>
                 </BodyShort>
                 <BodyShort as="li" size="small" aria-label="Arbeidstid">
-                    <TimerStartIcon aria-hidden />
-                    {cv.arbeidstidJobbprofil?.length > 0 ? (
-                        <MangeTekstelementerSeparertMedOg
-                            elementer={cv.arbeidstidJobbprofil.map((u) => u.arbeidstidKodeTekst)}
-                        />
-                    ) : (
-                        '-'
-                    )}
+                    <ClockIcon aria-hidden />
+                    <div>
+                        <BodyShort size="small">Arbeidstid</BodyShort>
+                        {cv.arbeidstidJobbprofil?.length > 0 ? (
+                            <MangeTekstelementerSeparertMedOg
+                                elementer={cv.arbeidstidJobbprofil.map(
+                                    (u) => u.arbeidstidKodeTekst
+                                )}
+                            />
+                        ) : (
+                            '-'
+                        )}
+                    </div>
                 </BodyShort>
-                <BodyShort as="li" size="small" aria-label="Oppstart">
-                    <HourglassIcon aria-hidden />
-                    {cv.oppstartKode ? (
-                        <MangeTekstelementerSeparertMedKomma
-                            elementer={[oppstartskoder[cv.oppstartKode]?.label]}
-                        />
-                    ) : (
-                        '-'
-                    )}
+                <BodyShort as="li" size="small" aria-label="Kan starte">
+                    <TimerStartIcon aria-hidden />
+                    <div>
+                        <BodyShort size="small">Kan starte</BodyShort>
+                        {cv.oppstartKode ? (
+                            <MangeTekstelementerSeparertMedKomma
+                                elementer={[oppstartskoder[cv.oppstartKode]?.label]}
+                            />
+                        ) : (
+                            '-'
+                        )}
+                    </div>
                 </BodyShort>
             </ul>
         </Kort>
