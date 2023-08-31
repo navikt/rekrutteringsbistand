a6f0b2d182b9ea3dbb199c6b97db4296abaf0175
Vilde Stefferud <33753494+vildestefferud@users.noreply.github.com>
2023-08-31 14:43:51 +0200

    Fiks design på fanene (#40)
    * Fiks design på fanene
    
    * Fiks mock
    
    * Endre lenke-komponent
    
    * Fiks css
    
    * Fjern borderen
    
    * wip Fiks styling til mobile first
    
    * Fullfør mobile-first design på faner
    
    * Deploy til dev
    
    * Fiks lenke
    
    ---------
    
    Co-authored-by: Kjetil Svalestuen <kjetil.svalestuen@nav.no>

--- src/kandidat/kandidatside/fraSøkUtenKontekst/FraSøkUtenKontekst.tsx
+++ src/kandidat/kandidatside/fraSøkUtenKontekst/FraSøkUtenKontekst.tsx
@@ -1,7 +1,6 @@
-import { MagnifyingGlassIcon, PersonPlusIcon } from '@navikt/aksel-icons';
-import { Button, Label, Tabs } from '@navikt/ds-react';
-import { FunctionComponent, ReactNode, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Button, Tabs } from '@navikt/ds-react';
+import { FunctionComponent, MouseEvent, ReactNode, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { Nettstatus } from 'felles/nettressurs';
 import { lenkeTilKandidatsøk } from '../../app/paths';
@@ -14,6 +13,7 @@ import Kandidatmeny from '../komponenter/meny/Kandidatmeny';
 import { hentØktFraKandidatsøk } from '../søkekontekst';
 import css from './FraSøkUtenKontekst.module.css';
 import LagreKandidaterIMineKandidatlisterModal from './lagre-kandidat-modal/LagreKandidatIMineKandidatlisterModal';
+import { TasklistIcon } from '@navikt/aksel-icons';
 
 type Props = {
     tabs: ReactNode;
@@ -24,10 +24,13 @@ type Props = {
 const FraSøkUtenKontekst: FunctionComponent<Props> = ({ tabs, kandidatnr, children }) => {
     useScrollTilToppen(kandidatnr);
 
+    const navigate = useNavigate();
     const [fane, setFane] = useFaner();
     const cv = useCv(kandidatnr);
     const kandidatnavigering = useNavigerbareKandidaterFraSøk(kandidatnr);
     const [visKandidatlisterModal, setVisKandidatlisterModal] = useState<boolean>(false);
+
+    const finnStillingUrl = `/stillingssok/${kandidatnr}?brukKriterierFraKandidat=true`;
 
     const økt = hentØktFraKandidatsøk();
 
@@ -39,6 +42,12 @@ const FraSøkUtenKontekst: FunctionComponent<Props> = ({ tabs, kandidatnr, child
         },
     ];
 
+    const handleFinnStillingClick = (event: MouseEvent) => {
+        event.preventDefault();
+
+        navigate(finnStillingUrl);
+    };
+
     return (
         <>
             <Kandidatheader
@@ -47,28 +56,21 @@ const FraSøkUtenKontekst: FunctionComponent<Props> = ({ tabs, kandidatnr, child
                 kandidatnavigering={kandidatnavigering}
                 brødsmulesti={brødsmulesti}
             />
-            <Tabs value={fane} onChange={setFane as any}>
+            <Tabs value={fane} onChange={setFane as any} className={css.tabs}>
                 <Kandidatmeny tabs={tabs} cv={cv}>
-                    <div className={css.knapper}>
-                        {cv.kind === Nettstatus.Suksess && (
-                            <Link
-                                to={`/stillingssok/${kandidatnr}?brukKriterierFraKandidat=true`}
-                                className="navds-button navds-button--secondary navds-button--small"
-                            >
-                                <MagnifyingGlassIcon />
-                                <Label as="span" size="small">
-                                    Finn stilling
-                                </Label>
-                            </Link>
-                        )}
-                        <Button
-                            size="small"
-                            icon={<PersonPlusIcon aria-hidden />}
-                            onClick={() => setVisKandidatlisterModal(true)}
-                        >
-                            Velg kandidatlister
+                    {cv.kind === Nettstatus.Suksess && (
+                        <Button as="a" href={finnStillingUrl} onClick={handleFinnStillingClick}>
+                            Finn stilling
                         </Button>
-                    </div>
+                    )}
+                    <Button
+                        as="a"
+                        variant="secondary"
+                        icon={<TasklistIcon aria-hidden />}
+                        onClick={() => setVisKandidatlisterModal(true)}
+                    >
+                        Legg til i lister
+                    </Button>
                 </Kandidatmeny>
                 <Tabs.Panel value={fane}>{children}</Tabs.Panel>
             </Tabs>
--- src/kandidat/komponenter/lenke-til-aktivitetsplan/LenkeTilAktivitetsplan.tsx
+++ src/kandidat/komponenter/lenke-til-aktivitetsplan/LenkeTilAktivitetsplan.tsx
@@ -1,18 +1,21 @@
 import { ExternalLinkIcon } from '@navikt/aksel-icons';
-import { Link } from '@navikt/ds-react';
+import { Button, Link } from '@navikt/ds-react';
 import { sendEvent } from 'felles/amplitude';
 import { api, post } from 'felles/api';
 import { erIkkeProd } from 'felles/miljø';
 import { Nettstatus } from 'felles/nettressurs';
-import { MouseEvent } from 'react';
+import { MouseEventHandler } from 'react';
 import { arbeidsrettetOppfølgingUrl } from '../../utils/eksterneUrler';
 
 type Props = {
     fnr: string;
+    somKnapp: boolean;
 };
 
-const LenkeTilAktivitetsplan = ({ fnr }: Props) => {
-    const handleClick = async (event: MouseEvent<HTMLAnchorElement>) => {
+const LenkeTilAktivitetsplan = ({ fnr, somKnapp }: Props) => {
+    const lenke = erIkkeProd ? arbeidsrettetOppfølgingUrl : `${arbeidsrettetOppfølgingUrl}/${fnr}`;
+
+    const handleClick: MouseEventHandler = async (event) => {
         event.preventDefault();
 
         sendEvent('cv_aktivitetsplan_lenke', 'klikk');
@@ -28,18 +31,30 @@ const LenkeTilAktivitetsplan = ({ fnr }: Props) => {
             );
         }
 
-        const element = event.target as HTMLAnchorElement;
-        window.open(element.href, '_blank');
+        window.open(lenke, '_blank');
     };
 
-    const lenke = erIkkeProd ? arbeidsrettetOppfølgingUrl : `${arbeidsrettetOppfølgingUrl}/${fnr}`;
-
-    return (
-        <Link target="_blank" href={lenke} onClick={handleClick}>
-            Se aktivitetsplan
-            <ExternalLinkIcon />
-        </Link>
-    );
+    if (somKnapp) {
+        return (
+            <Button
+                as="a"
+                target="_blank"
+                href={lenke}
+                variant="secondary"
+                onClick={handleClick}
+                icon={<ExternalLinkIcon aria-hidden />}
+            >
+                Se aktivitetsplan
+            </Button>
+        );
+    } else {
+        return (
+            <Link target="_blank" href={lenke} onClick={handleClick}>
+                Se aktivitetsplan
+                <ExternalLinkIcon />
+            </Link>
+        );
+    }
 };
 
 export default LenkeTilAktivitetsplan;
