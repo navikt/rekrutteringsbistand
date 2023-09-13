eb71644a791caf2bfdc89c28ffc9654dc057d682
Joar Aurdal <joar.aurdal@nav.no>
2023-09-13 14:06:03 +0200

    Fodselsnummeroppslag direkte til opensearch (#51)
    * Bruk opensearch query for fødselsnummer oppslag
    
    * Fiks feil med mapping av ikke funnet for legg til person med fnr
    
    * Ta bort logging brukt til debugging

--- src/kandidat/kandidatliste/modaler/legg-til-kandidat-modal/api.ts
+++ src/kandidat/kandidatliste/modaler/legg-til-kandidat-modal/api.ts
@@ -1,33 +1,31 @@
-import { api } from 'felles/api';
+import { api, post } from 'felles/api';
 import { Nettressurs, Nettstatus } from 'felles/nettressurs';
-import { SearchApiError, postHeaders } from '../../../api/fetchUtils';
+import { SearchApiError } from '../../../api/fetchUtils';
 import { KandidatLookup } from 'felles/domene/kandidat/Kandidat';
 import Synlighetsevaluering from 'felles/domene/synlighet/Synlighetsevaluering';
+import { EsQuery, EsResponse } from 'felles/domene/elastic/ElasticSearch';
 
 export const fetchKandidatMedFnr = async (fnr: string): Promise<Nettressurs<KandidatLookup>> => {
-    const url = `${api.kandidat}/veileder/kandidatsok/fnrsok`;
-    const body = JSON.stringify({ fnr });
-
     try {
-        const response = await fetch(url, {
-            method: 'POST',
-            body,
-            mode: 'cors',
-            credentials: 'include',
-            headers: postHeaders(),
-        });
+        const response = await post<EsResponse<KandidatLookup>>(api.kandidatsøk, byggQueryFnr(fnr));
 
-        if (response.ok) {
-            return {
-                kind: Nettstatus.Suksess,
-                data: await response.json(),
-            };
-        } else if (response.status === 404) {
-            return {
-                kind: Nettstatus.FinnesIkke,
-            };
+        if (response.kind === Nettstatus.Suksess) {
+            const data = response.data.hits.hits[0]?._source;
+            if (data) {
+                return {
+                    kind: Nettstatus.Suksess,
+                    data,
+                };
+            } else {
+                return {
+                    kind: Nettstatus.FinnesIkke,
+                };
+            }
         } else {
-            throw await response.text();
+            throw new SearchApiError({
+                message: `Uventet status: ${response.kind}`,
+                status: 400,
+            });
         }
     } catch (e) {
         throw new SearchApiError({
@@ -36,6 +34,19 @@ export const fetchKandidatMedFnr = async (fnr: string): Promise<Nettressurs<Kand
         });
     }
 };
+
+const byggQueryFnr = (
+    fodselsnummer: string
+): EsQuery<{
+    fodselsnummer: string;
+}> => ({
+    query: {
+        term: {
+            fodselsnummer,
+        },
+    },
+    size: 1,
+});
 
 export const fetchSynlighetsevaluering = async (
     fødselsnummer: string
--- src/stilling/stilling/legg-til-kandidat-modal/kandidatApi.ts
+++ src/stilling/stilling/legg-til-kandidat-modal/kandidatApi.ts
@@ -1,9 +1,10 @@
-import { api } from 'felles/api';
+import { api, post } from 'felles/api';
 import { ApiError } from '../../api/apiUtils';
 import { Nettressurs, Nettstatus } from 'felles/nettressurs';
 import { KandidatLookup } from 'felles/domene/kandidat/Kandidat';
 import Synlighetsevaluering from 'felles/domene/synlighet/Synlighetsevaluering';
 import Kandidatliste from 'felles/domene/kandidatliste/Kandidatliste';
+import { EsQuery, EsResponse } from 'felles/domene/elastic/ElasticSearch';
 
 export const fetchSynlighetsevaluering = async (
     fødselsnummer: string
@@ -46,29 +47,26 @@ export class KandidatSokError {
 }
 
 export const fetchKandidatMedFnr = async (fnr: string): Promise<Nettressurs<KandidatLookup>> => {
-    const url = `${api.kandidat}/veileder/kandidatsok/fnrsok`;
-    const body = JSON.stringify({ fnr });
-
     try {
-        const response = await fetch(url, {
-            method: 'POST',
-            body,
-            mode: 'cors',
-            credentials: 'include',
-            headers: postHeaders(),
-        });
+        const response = await post<EsResponse<KandidatLookup>>(api.kandidatsøk, byggQueryFnr(fnr));
 
-        if (response.ok) {
-            return {
-                kind: Nettstatus.Suksess,
-                data: await response.json(),
-            };
-        } else if (response.status === 404) {
-            return {
-                kind: Nettstatus.FinnesIkke,
-            };
+        if (response.kind === Nettstatus.Suksess) {
+            const data = response.data.hits.hits[0]?._source;
+            if (data) {
+                return {
+                    kind: Nettstatus.Suksess,
+                    data,
+                };
+            } else {
+                return {
+                    kind: Nettstatus.FinnesIkke,
+                };
+            }
         } else {
-            throw await response.text();
+            return {
+                kind: Nettstatus.Feil,
+                error: { message: 'Klarte ikke å hente data for kandidaten' },
+            };
         }
     } catch (e) {
         return {
@@ -78,16 +76,21 @@ export const fetchKandidatMedFnr = async (fnr: string): Promise<Nettressurs<Kand
     }
 };
 
+const byggQueryFnr = (
+    fodselsnummer: string
+): EsQuery<{
+    fodselsnummer: string;
+}> => ({
+    query: {
+        term: {
+            fodselsnummer,
+        },
+    },
+    size: 1,
+});
+
 export const fetchKandidatliste = (stillingsId: string): Promise<Kandidatliste> =>
     fetchJson(`${api.kandidat}/veileder/stilling/${stillingsId}/kandidatliste`);
-
-const postHeaders = () => {
-    return {
-        'Content-Type': 'application/json',
-        'X-XSRF-TOKEN': getCookie('XSRF-TOKEN'),
-        Accept: 'application/json',
-    };
-};
 
 async function fetchJson(url) {
     try {
@@ -109,10 +112,4 @@ async function fetchJson(url) {
 
 const throwError = (message?: string, status?: number) => {
     throw new KandidatSokError({ message, status });
-};
-
-const getCookie = (name) => {
-    const re = new RegExp(`${name}=([^;]+)`);
-    const match = re.exec(document.cookie);
-    return match !== null ? match[1] : '';
 };
