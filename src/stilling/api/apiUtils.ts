06efa099d78a00868f902af900c73367cee70cde
vildestefferud <qwartz.ms@hotmail.com>
2023-09-14 14:16:44 +0200

    Legg på logging

--- src/stilling/api/apiUtils.ts
+++ src/stilling/api/apiUtils.ts
@@ -59,7 +59,9 @@ const request = async (url: string, options?: RequestInit) => {
 
     try {
         response = await fetch(url, options);
+        console.log('inni request er response: ', response);
     } catch (e) {
+        console.log('inni request kaster feil: ', e);
         throw new ApiError('Network Error', 0);
     }
 
