7b276a0014235d177852f18c171fd0753d61cd92
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2023-09-18 13:48:51 +0200

    Oppdater når man oppdaterer tekster

--- src/kandidat/kandidatliste/modaler/presenter-kandidater/ForhåndsvisningAvEpost.tsx
+++ src/kandidat/kandidatliste/modaler/presenter-kandidater/ForhåndsvisningAvEpost.tsx
@@ -1,5 +1,6 @@
 import { api } from 'felles/api';
 import Kandidatliste from 'felles/domene/kandidatliste/Kandidatliste';
+import { useCallback, useEffect, useRef } from 'react';
 import css from './ForhåndsvisningAvEpost.module.css';
 
 type Props = {
@@ -8,19 +9,34 @@ type Props = {
 };
 
 const ForhåndsvisningAvEpost = ({ kandidatliste, melding }: Props) => {
-    const handleFerdigLastet = (iFrame: HTMLIFrameElement) => {
-        const iframeDocument = iFrame.contentWindow.document;
+    const iframeRef = useRef<HTMLIFrameElement>();
 
-        const tittelElement = iframeDocument.getElementById('tittel');
-        const stillingstittelElement = iframeDocument.getElementById('stillingstittel');
-        const tekstElement = iframeDocument.getElementById('tekst');
-        const avsenderElement = iframeDocument.getElementById('avsender');
+    const replacePlaceholders = useCallback(
+        (iframe: HTMLIFrameElement) => {
+            const iframeDocument = iframe.contentWindow.document;
 
-        if (tittelElement) tittelElement.innerText = kandidatliste.tittel;
-        if (stillingstittelElement) stillingstittelElement.innerText = kandidatliste.tittel;
-        if (tekstElement) tekstElement.innerHTML = melding;
-        if (avsenderElement) avsenderElement.innerHTML = kandidatliste.opprettetAv.navn;
+            const tittelElement = iframeDocument.getElementById('tittel');
+            const stillingstittelElement = iframeDocument.getElementById('stillingstittel');
+            const tekstElement = iframeDocument.getElementById('tekst');
+            const avsenderElement = iframeDocument.getElementById('avsender');
+
+            if (tittelElement) tittelElement.innerText = kandidatliste.tittel;
+            if (stillingstittelElement) stillingstittelElement.innerText = kandidatliste.tittel;
+            if (tekstElement) tekstElement.innerHTML = melding;
+            if (avsenderElement) avsenderElement.innerHTML = kandidatliste.opprettetAv.navn;
+        },
+        [kandidatliste.opprettetAv.navn, kandidatliste.tittel, melding]
+    );
+
+    const handleFerdigLastet = (iframe: HTMLIFrameElement) => {
+        iframeRef.current = iframe;
+
+        replacePlaceholders(iframe);
     };
+
+    useEffect(() => {
+        replacePlaceholders(iframeRef.current);
+    }, [replacePlaceholders]);
 
     return (
         <iframe
