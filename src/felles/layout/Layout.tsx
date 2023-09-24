10dfaa2d5cbaddaf18fcdc26702d82a3f3b6e4bf
Erlend Wiklem <erlend.wiklem@nav.no>
2023-09-24 21:55:08 +0200

    Ny layout

---
+++ src/felles/layout/Layout.tsx
@@ -0,0 +1,68 @@
+import { Accordion, Heading, Hide, Show } from '@navikt/ds-react';
+import * as React from 'react';
+
+import stil from './Layout.module.css';
+export interface ILayout {
+    hovedside: React.ReactNode | undefined;
+    tittel?: string;
+    ikon?: React.ReactNode;
+    altBanner?: React.ReactNode;
+    sidepanel?: React.ReactNode | undefined;
+    bannerKnapp?: React.ReactNode | undefined;
+}
+
+const Layout: React.FC<ILayout> = ({
+    tittel,
+    ikon,
+    hovedside,
+    sidepanel,
+    altBanner,
+    bannerKnapp,
+}) => {
+    return (
+        <div className={stil.wrapper}>
+            <div className={stil.layout}>
+                {altBanner ? (
+                    altBanner
+                ) : (
+                    <header>
+                        <div className={stil.banner}>
+                            <div className={stil.bannerTittel}>
+                                <div className={stil.ikon}>{ikon}</div>
+                                <Heading level="2" size="large">
+                                    {tittel}
+                                </Heading>
+                            </div>
+                            {bannerKnapp}
+                        </div>
+                    </header>
+                )}
+                <div className={stil.container}>
+                    {sidepanel && (
+                        <aside className={stil.sidebar}>
+                            <Show above="md">{sidepanel}</Show>
+                            <Hide above="md">
+                                <Accordion>
+                                    <Accordion.Item>
+                                        <Accordion.Header>Sidepanel</Accordion.Header>
+                                        <Accordion.Content>{sidepanel}</Accordion.Content>
+                                    </Accordion.Item>
+                                </Accordion>
+                            </Hide>
+                        </aside>
+                    )}
+                    <main className={stil.sideinnhold}>{hovedside}</main>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Layout;
+
+// Margins:
+// >1280dp -> 2,5rem = 40px
+// >1024 = 32px
+// >768 = 24px
+// <767 = 20px
+// Bunn 80px
