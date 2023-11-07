d8808602efe4988f77c93a34226e12e812100651
Joar Aurdal <joar.aurdal@nav.no>
2023-11-07 12:52:14 +0100

    Sjekk om vi har suksessmelding på kandidatliste før vi viser kandidatlisteknapper i stilling

--- src/stilling/stilling/header/Stillingsheader.tsx
+++ src/stilling/stilling/header/Stillingsheader.tsx
@@ -2,16 +2,20 @@ import { ReactNode } from 'react';
 import Kandidathandlinger from '../kandidathandlinger/Kandidathandlinger';
 import css from './Stillingsheader.module.css';
 import Kandidatliste from 'felles/domene/kandidatliste/Kandidatliste';
-import { Nettressurs } from 'felles/nettressurs';
+import { Nettressurs, Nettstatus } from 'felles/nettressurs';
 
 type Props = {
     kandidatliste: Nettressurs<Kandidatliste>;
     children: ReactNode;
 };
 
+// TODO: Vi trenger ikke sende inn nettressurs om vi kun viser komponenten når vi har kind = success
 const Stillingsheader = ({ kandidatliste, children }: Props) => (
     <div className={css.stillingsheader}>
-        <Kandidathandlinger kandidatliste={kandidatliste} />
+        {kandidatliste.kind === Nettstatus.Suksess && (
+            <Kandidathandlinger kandidatliste={kandidatliste} />
+        )}
+
         <div className={css.knapper}>{children}</div>
     </div>
 );
