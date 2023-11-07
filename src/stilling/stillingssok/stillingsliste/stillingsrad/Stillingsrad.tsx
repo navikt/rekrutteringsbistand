7d34805c2a5ce99230147fca9d89bdbfb08709bb
Erlend Wiklem <erlend.wiklem@nav.no>
2023-11-07 13:13:44 +0100

    Må være eier av stilling for å se lenke til kandidater

--- src/stilling/stillingssok/stillingsliste/stillingsrad/Stillingsrad.tsx
+++ src/stilling/stillingssok/stillingsliste/stillingsrad/Stillingsrad.tsx
@@ -9,7 +9,7 @@ import {
     EsStilling,
     stillingErUtløpt,
 } from 'felles/domene/stilling/EsStilling';
-import { Geografi, Privacy } from 'felles/domene/stilling/Stilling';
+import { Geografi, Privacy, Stillingskategori } from 'felles/domene/stilling/Stilling';
 import RekBisKortStilling from '../../../../felles/komponenter/rekbis-kort/RekBisKortStilling';
 import TekstlinjeMedIkon from '../../../../felles/komponenter/tekstlinje-med-ikon/TekstlinjeMedIkon';
 import { REDIGERINGSMODUS_QUERY_PARAM } from '../../../stilling/Stilling';
@@ -62,6 +62,12 @@ const Stillingsrad: FunctionComponent<Props> = ({
 
     const publisertDato = konverterTilPresenterbarDato(stilling.published);
     const utløpsDato = konverterTilPresenterbarDato(stilling.expires);
+
+    const erFormidling =
+        rekrutteringsbistandstilling.stillingsinfo?.stillingskategori ===
+        Stillingskategori.Formidling;
+
+    const kanSeKandidatliste = erEier || !erFormidling;
 
     return (
         <RekBisKortStilling
@@ -133,7 +139,8 @@ const Stillingsrad: FunctionComponent<Props> = ({
                             Rediger
                         </Link>
                     )}
-                    {rekrutteringsbistandstilling.stilling.publishedByAdmin &&
+                    {kanSeKandidatliste &&
+                        rekrutteringsbistandstilling.stilling.publishedByAdmin &&
                         skalViseLenkeTilKandidatliste(rekrutteringsbistandstilling) && (
                             <Link className={css.lenke} to={lagUrlTilKandidatliste(stilling)}>
                                 Vis kandidater
