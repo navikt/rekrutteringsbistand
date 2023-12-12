e91c0a1013c2d8b3d0358146d6f2dcf010af9411
Erlend Wiklem <erlend.wiklem@nav.no>
2023-12-12 21:48:13 +0100

    finskrive funksjoner

--- src/stilling/stillingssok/api/queries/stillingskategori.ts
+++ src/stilling/stillingssok/api/queries/stillingskategori.ts
@@ -1,23 +1,37 @@
 import { Stillingskategori } from 'felles/domene/stilling/Stilling';
 
-export const stillingskategori = (stillingskategori: Set<Stillingskategori>) => {
-    const skalInkluderes = Array.from(stillingskategori).map((kategori) => ({
+const mapKategori = (stillingskategori: Set<Stillingskategori>) =>
+    Array.from(stillingskategori).map((kategori) => ({
         term: {
             'stillingsinfo.stillingskategori': kategori,
         },
     }));
 
+const skalInkludere = (stillingskategori: Set<Stillingskategori>) => {
+    return [
+        {
+            bool: {
+                should: mapKategori(stillingskategori),
+            },
+        },
+    ];
+};
+
+const skalEkskludere = (stillingskategori: Set<Stillingskategori>) => {
+    return [
+        {
+            bool: {
+                must_not: mapKategori(stillingskategori),
+            },
+        },
+    ];
+};
+
+export const stillingskategori = (stillingskategori: Set<Stillingskategori>) => {
     // Formidlingsside
     if (stillingskategori.has(Stillingskategori.Formidling)) {
-        return [
-            {
-                bool: {
-                    should: skalInkluderes,
-                },
-            },
-        ];
+        return skalInkludere(stillingskategori);
     }
-
     // Stillingsside
     // ingen valgt eller begge valgt (Stilling og Jobbmesse)
     if (
@@ -25,56 +39,14 @@ export const stillingskategori = (stillingskategori: Set<Stillingskategori>) => 
         (stillingskategori.has(Stillingskategori.Stilling) &&
             stillingskategori.has(Stillingskategori.Jobbmesse))
     ) {
-        return [
-            {
-                bool: {
-                    must_not: [
-                        {
-                            term: {
-                                'stillingsinfo.stillingskategori': Stillingskategori.Formidling,
-                            },
-                        },
-                    ],
-                },
-            },
-        ];
+        return skalEkskludere(new Set([Stillingskategori.Formidling]));
     }
-
     // Stilling valgt
     if (stillingskategori.has(Stillingskategori.Stilling)) {
-        return [
-            {
-                bool: {
-                    must_not: [
-                        {
-                            term: {
-                                'stillingsinfo.stillingskategori': Stillingskategori.Jobbmesse,
-                            },
-                        },
-                        {
-                            term: {
-                                'stillingsinfo.stillingskategori': Stillingskategori.Formidling,
-                            },
-                        },
-                    ],
-                },
-            },
-        ];
+        return skalEkskludere(new Set([Stillingskategori.Formidling, Stillingskategori.Jobbmesse]));
     }
     // Jobbmesse valgt
     if (stillingskategori.has(Stillingskategori.Jobbmesse)) {
-        return [
-            {
-                bool: {
-                    must: [
-                        {
-                            term: {
-                                'stillingsinfo.stillingskategori': Stillingskategori.Jobbmesse,
-                            },
-                        },
-                    ],
-                },
-            },
-        ];
+        return skalInkludere(stillingskategori);
     }
 };
