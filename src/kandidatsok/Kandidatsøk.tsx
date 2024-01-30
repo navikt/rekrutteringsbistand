f119fe8dc8beba81514ae842620f667086a823c3
Erlend Wiklem <erlend.wiklem@nav.no>
2024-01-30 13:05:40 +0100

    Oppsummering ved lagring av kandidat i kandidatliste

--- src/kandidatsok/Kandidatsøk.tsx
+++ src/kandidatsok/Kandidatsøk.tsx
@@ -1,12 +1,9 @@
 import { useEffect, useState } from 'react';
 
-import { Alert } from '@navikt/ds-react';
 import { KandidatTilKandidatsøk } from 'felles/domene/kandidat/Kandidat';
-import Kandidatliste from 'felles/domene/kandidatliste/Kandidatliste';
 import { InnloggetBruker } from 'felles/hooks/useInnloggetBruker';
 import { ReactComponent as Piktogram } from 'felles/komponenter/piktogrammer/finn-kandidater.svg';
 import Layout from '../felles/komponenter/layout/Layout';
-import { Nettstatus } from '../felles/nettressurs';
 import css from './Kandidatsøk.module.css';
 import Filter from './filter/Filter';
 import TømFiltre from './filter/TømFiltre';
@@ -62,23 +59,6 @@ const Kandidatsøk = ({
         );
     };
 
-    const onSuccessLagretKandidaterISpesifikkKandidatliste = (
-        oppdatertKandidatliste: Kandidatliste
-    ) => {
-        fjernMarkering();
-        kontekstAvKandidatlisteEllerStilling?.setOppdatertKandidatliste(oppdatertKandidatliste);
-    };
-
-    if (
-        kontekstAvKandidatlisteEllerStilling?.kandidatliste?.kind === Nettstatus.Feil ||
-        kontekstAvKandidatlisteEllerStilling?.kandidatliste?.kind === Nettstatus.FinnesIkke
-    ) {
-        return (
-            <Layout>
-                <Alert variant="error">Du har ikke tilgang til kandidatlisten</Alert>
-            </Layout>
-        );
-    }
 
     return (
         <Layout
@@ -119,7 +99,6 @@ const Kandidatsøk = ({
                     onClose={() => setAktivModal(Modal.IngenModal)}
                     markerteKandidater={markerteKandidater}
                     kontekstAvKandidatlisteEllerStilling={kontekstAvKandidatlisteEllerStilling}
-                    onSuksess={onSuccessLagretKandidaterISpesifikkKandidatliste}
                 />
             )}
         </Layout>
