d4ab8cd729f2ab142fe5bed509e95af275939894
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2024-02-27 11:52:50 +0100

    Vis varsel om nedetid

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -5,10 +5,15 @@ import Dekoratør from './modiadekoratør/Modiadekoratør';
 import Placeholder from './modiadekoratør/Placeholder';
 import Navigeringsmeny from './navigeringsmeny/Navigeringsmeny';
 import useAmplitude from './useAmplitude';
+import React, { useState } from 'react';
+import { Alert } from '@navikt/ds-react';
 
 const Header = () => {
     const { navKontor, setNavKontor } = useNavKontor();
     const [mockAktiv] = useLocalStorageToggle('Mock modia');
+    const [visNedetidvarsel, setVisNedetidvarsel] = useState(
+        () => window.localStorage.getItem('nedetid pg11-oppgradering') !== 'false'
+    );
     useAmplitude(navKontor);
 
     const Modiadekoratør = import.meta.env.DEV && mockAktiv ? Placeholder : Dekoratør;
@@ -16,6 +21,20 @@ const Header = () => {
     return (
         <>
             <Modiadekoratør navKontor={navKontor} onNavKontorChange={setNavKontor} />
+            {visNedetidvarsel ? (
+                <Alert
+                    variant="warning"
+                    fullWidth={true}
+                    closeButton
+                    onClose={() => {
+                        setVisNedetidvarsel(false);
+                        window.localStorage.setItem('nedetid pg11-oppgradering', 'false');
+                    }}
+                >
+                    Rekrutteringsbistand vil være skrudd av i dag (XX. februar) mellom XX:XX og
+                    YY:YY for vedlikehold. Vi beklager ulempen.
+                </Alert>
+            ) : null}
             <Navigeringsmeny />
             <Outlet />
         </>
