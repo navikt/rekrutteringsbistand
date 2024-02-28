40c246b1d78153a673c3cefc73fe4771e6a61811
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2024-02-28 11:39:56 +0100

    Lag alerten litt mindre, så det er lettere å se at man kan krysse den ut

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -21,20 +21,22 @@ const Header = () => {
     return (
         <>
             <Modiadekoratør navKontor={navKontor} onNavKontorChange={setNavKontor} />
-            {visNedetidvarsel ? (
-                <Alert
-                    variant="warning"
-                    closeButton
-                    onClose={() => {
-                        setVisNedetidvarsel(false);
-                        window.localStorage.setItem('nedetid pg11-oppgradering', 'false');
-                    }}
-                >
-                    Sending av SMS og visning av kontorstatistikk vil være skrudd av torsdag 29.
-                    februar fra 16:00 til 17:00 for vedlikehold. Vi beklager ulempen.
-                </Alert>
-            ) : null}
             <Navigeringsmeny />
+            {visNedetidvarsel ? (
+                <div style={{ maxWidth: '60rem', margin: '8px auto' }}>
+                    <Alert
+                        variant="warning"
+                        closeButton
+                        onClose={() => {
+                            setVisNedetidvarsel(false);
+                            window.localStorage.setItem('nedetid pg11-oppgradering', 'false');
+                        }}
+                    >
+                        Sending av SMS og visning av kontorstatistikk vil være skrudd av torsdag 29.
+                        februar fra 16:00 til 17:00 for vedlikehold. Vi beklager ulempen.
+                    </Alert>
+                </div>
+            ) : null}
             <Outlet />
         </>
     );
