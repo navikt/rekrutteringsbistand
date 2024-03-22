94797f127f6c705f1a3324d75003556152f353dc
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2024-03-22 15:35:06 +0100

    post/get fetchere med schema

--- src/api/fetcher.ts
+++ src/api/fetcher.ts
@@ -1,4 +1,13 @@
+import { z, ZodSchema } from 'zod';
+
 const basePath = import.meta.env.VITEST ? 'http://localhost:3000' : '';
+
+export const getAPIwithSchema = <T>(schema: ZodSchema<T>): ((url: string) => Promise<T>) => {
+    return async (url: string) => {
+        const data = getAPI(url);
+        return schema.parse(data);
+    };
+};
 
 export const getAPI = async (url: string) => {
     const response = await fetch(basePath + url, {
@@ -48,3 +57,36 @@ export const postApiResponse = (url: string, body: any) =>
         },
         body: JSON.stringify(body),
     });
+
+export type postApiProps = {
+    url: string;
+    body: any;
+};
+
+const esResponseDto = z.object({
+    hits: z.object({
+        hits: z.array(
+            z.object({
+                _source: z.any(),
+            })
+        ),
+    }),
+});
+export const postApiWithSchemaEs = <T>(
+    schema: ZodSchema<T>
+): ((props: postApiProps) => Promise<T>) => {
+    return async (props) => {
+        const data: any = await postApi(props.url, props.body);
+        const parsedData = esResponseDto.parse(data);
+        return schema.parse(parsedData.hits.hits[0]._source);
+    };
+};
+
+export const postApiWithSchema = <T>(
+    schema: ZodSchema<T>
+): ((props: postApiProps) => Promise<T>) => {
+    return async (props) => {
+        const data = await postApi(props.url, props.body);
+        return schema.parse(data);
+    };
+};
