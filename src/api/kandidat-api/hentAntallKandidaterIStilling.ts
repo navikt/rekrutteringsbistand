0cb96fad2ba7703ffb5e52f6f22521189836676a
Joar Aurdal <joar.aurdal@nav.no>
2024-05-14 10:00:49 +0200

    Fullfør antall kandidater for stilling

---
+++ src/api/kandidat-api/hentAntallKandidaterIStilling.ts
@@ -0,0 +1,31 @@
+/**
+ * Endepunkt /kandidatsok-api/api/suggest/arena-kandidatnr
+ */
+import { HttpResponse, http } from 'msw';
+import useSWRImmutable from 'swr';
+import { z } from 'zod';
+import { getAPIwithSchema } from '../fetcher';
+
+const kandidatlisterEndepunkt = '/kandidat-api/veileder/kandidatlister';
+
+export const antallKandidaterSchema = z.object({
+    antallKandidater: z.number().nullable(),
+});
+
+export interface hentAntallKandidaterIListeProps {
+    kandidatlisteId: string;
+}
+
+export const useHentAntallKandidaterIStilling = (props: hentAntallKandidaterIListeProps) => {
+    return useSWRImmutable(
+        `${kandidatlisterEndepunkt}/${props.kandidatlisteId}/antallKandidater`,
+        getAPIwithSchema(antallKandidaterSchema)
+    );
+};
+
+export const hentAntallKandidaterIStillingMockMsw = http.get(
+    `${kandidatlisterEndepunkt}/:kandidatlisteId/antallKandidater`,
+    async (_) => {
+        return HttpResponse.json({ antallKandidater: 2 });
+    }
+);
---
+++ src/stilling/stilling/forhåndsvisning/antall-kandidater/AntallKandidater.tsx
@@ -0,0 +1,19 @@
+import { Alert, Loader } from '@navikt/ds-react';
+import { useHentAntallKandidaterIStilling } from '../../../../api/kandidat-api/hentAntallKandidaterIStilling';
+
+type Props = {
+    kandidatelisteId: string;
+};
+
+export const AntallKandidaterIStilling = ({ kandidatelisteId }: Props) => {
+    const { data, isLoading } = useHentAntallKandidaterIStilling({
+        kandidatlisteId: kandidatelisteId,
+    });
+    return isLoading ? (
+        <Loader />
+    ) : (
+        <Alert variant="info">
+            Antall kandidater allerede registrert for stillingen: {data?.antallKandidater}
+        </Alert>
+    );
+};
