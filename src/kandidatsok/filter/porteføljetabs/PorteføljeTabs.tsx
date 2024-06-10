e65f10d1dddee2f1a8e703662ce39a04310a24c1
Erlend Wiklem <erlend.wiklem@nav.no>
2024-06-10 15:15:15 +0200

    Skjul MineKontorer hvis bruker bare har ett kontor

--- src/kandidatsok/filter/porteføljetabs/PorteføljeTabs.tsx
+++ src/kandidatsok/filter/porteføljetabs/PorteføljeTabs.tsx
@@ -1,6 +1,7 @@
-import { Tabs } from '@navikt/ds-react';
+import { Loader, Tabs } from '@navikt/ds-react';
 import { ReactNode, useContext } from 'react';
 import { Portefølje } from '../../../api/kandidat-søk-api/kandidatsøk';
+import { useDecorator } from '../../../api/modiacontextholder/decorator';
 import { ApplikasjonContext } from '../../../felles/ApplikasjonContext';
 import { Rolle } from '../../../felles/tilgangskontroll/Roller';
 import { TilgangskontrollForInnhold } from '../../../felles/tilgangskontroll/TilgangskontrollForInnhold';
@@ -12,11 +13,14 @@ import VelgKontorTab from './VelgKontorTab';
 const PorteføljeTabs = ({ children }: { children: ReactNode }) => {
     const { søkekriterier, setSearchParam } = useSøkekriterier();
     const { tilgangskontrollErPå } = useContext(ApplikasjonContext);
-
+    const { data, isLoading } = useDecorator();
     const velgPortefølje = (portefølje: string) => {
         setSearchParam(FilterParam.Portefølje, portefølje);
     };
 
+    if (isLoading) {
+        return <Loader />;
+    }
     return (
         <Tabs value={søkekriterier.portefølje} onChange={velgPortefølje}>
             <Tabs.List>
@@ -26,27 +30,31 @@ const PorteføljeTabs = ({ children }: { children: ReactNode }) => {
                 >
                     <Tabs.Tab value={Portefølje.ALLE} label="Alle" />
                 </TilgangskontrollForInnhold>
-                <TilgangskontrollForInnhold
-                    skjulVarsel
-                    kreverEnAvRollene={[
-                        Rolle.AD_GRUPPE_REKRUTTERINGSBISTAND_ARBEIDSGIVERRETTET,
-                        Rolle.AD_GRUPPE_REKRUTTERINGSBISTAND_JOBBSOKERRETTET,
-                    ]}
-                >
-                    {tilgangskontrollErPå && (
-                        <Tabs.Tab value={Portefølje.MINE_KONTORER} label="Mine kontorer" />
-                    )}
-                </TilgangskontrollForInnhold>
+                {data?.enheter && data.enheter.length > 1 && (
+                    <TilgangskontrollForInnhold
+                        skjulVarsel
+                        kreverEnAvRollene={[
+                            Rolle.AD_GRUPPE_REKRUTTERINGSBISTAND_ARBEIDSGIVERRETTET,
+                            Rolle.AD_GRUPPE_REKRUTTERINGSBISTAND_JOBBSOKERRETTET,
+                        ]}
+                    >
+                        {tilgangskontrollErPå && (
+                            <Tabs.Tab value={Portefølje.MINE_KONTORER} label="Mine kontorer" />
+                        )}
+                    </TilgangskontrollForInnhold>
+                )}
                 <Tabs.Tab value={Portefølje.MINE_BRUKERE} label="Mine brukere" />
-                <TilgangskontrollForInnhold
-                    skjulVarsel
-                    kreverEnAvRollene={[
-                        Rolle.AD_GRUPPE_REKRUTTERINGSBISTAND_ARBEIDSGIVERRETTET,
-                        Rolle.AD_GRUPPE_REKRUTTERINGSBISTAND_JOBBSOKERRETTET,
-                    ]}
-                >
-                    <Tabs.Tab value={Portefølje.MITT_KONTOR} label="Mitt kontor" />
-                </TilgangskontrollForInnhold>
+                {data?.enheter && data.enheter.length > 0 && (
+                    <TilgangskontrollForInnhold
+                        skjulVarsel
+                        kreverEnAvRollene={[
+                            Rolle.AD_GRUPPE_REKRUTTERINGSBISTAND_ARBEIDSGIVERRETTET,
+                            Rolle.AD_GRUPPE_REKRUTTERINGSBISTAND_JOBBSOKERRETTET,
+                        ]}
+                    >
+                        <Tabs.Tab value={Portefølje.MITT_KONTOR} label="Mitt kontor" />
+                    </TilgangskontrollForInnhold>
+                )}
                 <TilgangskontrollForInnhold
                     skjulVarsel
                     kreverEnAvRollene={[Rolle.AD_GRUPPE_REKRUTTERINGSBISTAND_ARBEIDSGIVERRETTET]}
