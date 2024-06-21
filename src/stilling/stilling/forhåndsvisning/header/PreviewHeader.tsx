c19440a67a978d618a6fd169e49efe65a440f94b
Wiklem <erlend.wiklem@nav.no>
2024-06-21 10:08:42 +0200

    Ny PreviewHeader, fetch id (#226)

--- src/stilling/stilling/forhåndsvisning/header/PreviewHeader.tsx
+++ src/stilling/stilling/forhåndsvisning/header/PreviewHeader.tsx
@@ -1,144 +1,101 @@
 import { DocPencilIcon, PrinterSmallIcon } from '@navikt/aksel-icons';
 import { Button } from '@navikt/ds-react';
-import React from 'react';
-import { connect } from 'react-redux';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-import Stilling, { Stillingsinfo, System } from 'felles/domene/stilling/Stilling';
+import { System } from '../../../../felles/domene/stilling/Stilling';
+import { Rolle } from '../../../../felles/tilgangskontroll/Roller';
+import { TilgangskontrollForInnhold } from '../../../../felles/tilgangskontroll/TilgangskontrollForInnhold';
 import { State } from '../../../redux/store';
-import { StillingsinfoState } from '../../../stillingsinfo/stillingsinfoReducer';
 import { COPY_AD_FROM_MY_ADS, EDIT_AD, LEGG_TIL_I_MINE_STILLINGER } from '../../adReducer';
 import Stillingsheader from '../../header/Stillingsheader';
 import EksternStillingAdvarsel from './EksternStillingAdvarsel';
 import OpprettKandidatlisteModal from './OpprettKandidatlisteModal';
-import { TilgangskontrollForInnhold } from '../../../../felles/tilgangskontroll/TilgangskontrollForInnhold';
-import { Rolle } from '../../../../felles/tilgangskontroll/Roller';
 
-type Props = {
-    stilling: Stilling;
-    stillingsinfoData: Stillingsinfo;
-    stillingsinfo: StillingsinfoState;
-    limitedAccess: boolean;
-    copyAd: (uuid: string) => void;
-    editAd: () => void;
-    leggTilIMineStillinger: () => void;
+interface IPreviewHeader {
     erEier: boolean;
-    kandidatlisteId: string;
-};
+    refetchKandidatlisteId: () => void;
+}
 
-class PreviewMenu extends React.Component<Props> {
-    declare state: {
-        opprettKandidatlisteModalÅpen: boolean;
+const PreviewHeader: React.FC<IPreviewHeader> = ({ erEier, refetchKandidatlisteId }) => {
+    const dispatch = useDispatch();
+    const { stilling, stillingsinfoData, limitedAccess } = useSelector((state: State) => ({
+        stilling: state.adData,
+        stillingsinfoData: state.stillingsinfoData,
+        stillingsinfo: state.stillingsinfo,
+        limitedAccess: state.adData?.createdBy !== System.Rekrutteringsbistand,
+    }));
+    const [opprettKandidatlisteModalÅpen, setOpprettKandidatlisteModalÅpen] = useState(false);
+
+    const onEditAdClick = () => {
+        dispatch({ type: EDIT_AD });
     };
 
-    constructor(props: Props) {
-        super(props);
-        this.state = {
-            opprettKandidatlisteModalÅpen: false,
-        };
-    }
-
-    onEditAdClick = () => {
-        this.props.editAd();
+    const onCopyAdClick = () => {
+        dispatch({ type: COPY_AD_FROM_MY_ADS, uuid: stilling?.uuid });
     };
 
-    onCopyAdClick = () => {
-        this.props.copyAd(this.props.stilling.uuid);
-    };
-
-    onPrintClick = () => {
+    const onPrintClick = () => {
         window.print();
     };
 
-    onLeggTilIMineStillingerClick = () => {
-        this.setState({
-            opprettKandidatlisteModalÅpen: true,
-        });
+    const onLeggTilIMineStillingerClick = () => {
+        setOpprettKandidatlisteModalÅpen(true);
     };
 
-    lukkOpprettKandidatlisteModal = () => {
-        this.setState({
-            opprettKandidatlisteModalÅpen: false,
-        });
+    const lukkOpprettKandidatlisteModal = () => {
+        setOpprettKandidatlisteModalÅpen(false);
     };
 
-    bekreftOpprettKandidatliste = () => {
-        this.props.leggTilIMineStillinger();
-        this.lukkOpprettKandidatlisteModal();
+    const bekreftOpprettKandidatliste = () => {
+        dispatch({ type: LEGG_TIL_I_MINE_STILLINGER });
+        refetchKandidatlisteId();
+        lukkOpprettKandidatlisteModal();
     };
 
-    render() {
-        const { limitedAccess, stillingsinfoData } = this.props;
+    const kanOverfoereStilling =
+        stillingsinfoData && limitedAccess && !stillingsinfoData.eierNavident;
 
-        const kanOverfoereStilling =
-            stillingsinfoData && limitedAccess && !stillingsinfoData.eierNavident;
+    return (
+        <>
+            <Stillingsheader>
+                <TilgangskontrollForInnhold
+                    kreverEnAvRollene={[Rolle.AD_GRUPPE_REKRUTTERINGSBISTAND_ARBEIDSGIVERRETTET]}
+                    skjulVarsel
+                >
+                    {!limitedAccess && erEier && (
+                        <Button onClick={onEditAdClick} size="small" icon={<DocPencilIcon />}>
+                            Rediger
+                        </Button>
+                    )}
+                    {!limitedAccess && (
+                        <Button onClick={onCopyAdClick} size="small" icon={<DocPencilIcon />}>
+                            Kopier
+                        </Button>
+                    )}
+                    {kanOverfoereStilling && (
+                        <Button onClick={onLeggTilIMineStillingerClick} size="small">
+                            Opprett kandidatliste
+                        </Button>
+                    )}
+                </TilgangskontrollForInnhold>
+                <Button
+                    variant="secondary"
+                    onClick={onPrintClick}
+                    size="small"
+                    icon={<PrinterSmallIcon />}
+                >
+                    Skriv ut
+                </Button>
+            </Stillingsheader>
+            {limitedAccess && <EksternStillingAdvarsel />}
+            <OpprettKandidatlisteModal
+                åpen={opprettKandidatlisteModalÅpen}
+                onClose={lukkOpprettKandidatlisteModal}
+                onBekreft={bekreftOpprettKandidatliste}
+            />
+        </>
+    );
+};
 
-        return (
-            <>
-                <Stillingsheader>
-                    <TilgangskontrollForInnhold
-                        kreverEnAvRollene={[
-                            Rolle.AD_GRUPPE_REKRUTTERINGSBISTAND_ARBEIDSGIVERRETTET,
-                        ]}
-                        skjulVarsel
-                    >
-                        {!limitedAccess && this.props.erEier && (
-                            <Button
-                                onClick={this.onEditAdClick}
-                                size="small"
-                                icon={<DocPencilIcon />}
-                            >
-                                Rediger
-                            </Button>
-                        )}
-                        {!limitedAccess && (
-                            <Button
-                                onClick={this.onCopyAdClick}
-                                size="small"
-                                icon={<DocPencilIcon />}
-                            >
-                                Kopier
-                            </Button>
-                        )}
-
-                        {kanOverfoereStilling && (
-                            <Button onClick={this.onLeggTilIMineStillingerClick} size="small">
-                                Opprett kandidatliste
-                            </Button>
-                        )}
-                    </TilgangskontrollForInnhold>
-                    <Button
-                        variant="secondary"
-                        onClick={this.onPrintClick}
-                        size="small"
-                        icon={<PrinterSmallIcon />}
-                    >
-                        Skriv ut
-                    </Button>
-                </Stillingsheader>
-                {limitedAccess && <EksternStillingAdvarsel />}
-
-                <OpprettKandidatlisteModal
-                    åpen={this.state.opprettKandidatlisteModalÅpen}
-                    onClose={this.lukkOpprettKandidatlisteModal}
-                    onBekreft={this.bekreftOpprettKandidatliste}
-                />
-            </>
-        );
-    }
-}
-
-const mapStateToProps = (state: State) => ({
-    stillingsinfoData: state.stillingsinfoData,
-    stillingsinfo: state.stillingsinfo,
-    stilling: state.adData,
-    limitedAccess: state.adData?.createdBy !== System.Rekrutteringsbistand,
-});
-
-const mapDispatchToProps = (dispatch: (action: any) => void) => ({
-    editAd: () => dispatch({ type: EDIT_AD }),
-    copyAd: (uuid: string) => dispatch({ type: COPY_AD_FROM_MY_ADS, uuid }),
-    leggTilIMineStillinger: () => dispatch({ type: LEGG_TIL_I_MINE_STILLINGER }),
-});
-
-// @ts-ignore TODO: written before strict-mode enabled
-export default connect(mapStateToProps, mapDispatchToProps)(PreviewMenu);
+export default PreviewHeader;
