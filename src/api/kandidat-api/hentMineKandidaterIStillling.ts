ab2190209118e7e2efac19386ad4a5f2b4582803
Joar Aurdal <joar.aurdal@nav.no>
2024-07-08 12:06:52 +0200

    Lag usemetode for å hente mine kandidater i stillling

---
+++ src/api/kandidat-api/hentMineKandidaterIStillling.ts
@@ -0,0 +1,31 @@
+/**
+ * Endepunkt /kandidatsok-api/veileder/kandidatlister/:kandidatlisteId/antallKandidater
+ */
+import { HttpResponse, http } from 'msw';
+import useSWRImmutable from 'swr';
+import { z } from 'zod';
+import { getAPIwithSchema } from '../fetcher';
+
+const hentMineKandidaterIStillingEndepunkt = (stillingsuuid: string) => {
+    return `/kandidat-api/stilling/${stillingsuuid}/minekandidatnummer`;
+};
+
+export const hentMineKandidaterIStillingSchema = z.array(z.string());
+
+export interface mineKandidaterIStillingProps {
+    stillingsuuid: string;
+}
+
+export const useHentMineKandidaterIStilling = (props: mineKandidaterIStillingProps) => {
+    return useSWRImmutable(
+        hentMineKandidaterIStillingEndepunkt(props.stillingsuuid),
+        getAPIwithSchema(hentMineKandidaterIStillingSchema)
+    );
+};
+
+export const hentMineKandidaterIStillingMockMsw = http.get(
+    hentMineKandidaterIStillingEndepunkt(':stillingsuuid'),
+    async (_) => {
+        return HttpResponse.json(['pam123', 'pam234']);
+    }
+);
