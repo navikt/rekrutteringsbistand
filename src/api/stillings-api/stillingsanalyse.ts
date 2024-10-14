d766d0be9632c7d55db5a7e43f99a119ae2964d1
Joar Aurdal <joar.aurdal@nav.no>
2024-10-14 16:03:42 +0200

    Deploy openai mock

--- src/api/stillings-api/stillingsanalyse.ts
+++ src/api/stillings-api/stillingsanalyse.ts
@@ -44,5 +44,7 @@ export const useStillingsanalyse = (props: StillingsanalyseRequestDTO, vis: bool
 };
 
 export const stillingsanalyseMockMsw = http.post(stillingsanalyseEndepunkt, async (_) => {
-    HttpResponse.json({ sensitiv: false, begrunnelse: '' });
+    HttpResponse.json(mock);
 });
+
+const mock: StillingsanalyseDTO = { sensitiv: false, begrunnelse: '' };
