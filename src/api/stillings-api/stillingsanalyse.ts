db23fe00cfd901438fccde798715e6672d6286cf
Joar Aurdal <joar.aurdal@nav.no>
2024-10-16 17:20:59 +0200

    Openai evaluering (#246)
    * Legg til modal for analyse av stilling
    
    * Legg til usemetode for analyse av stilling
    
    * Deploy openai mock
    
    * Fiks mock
    
    * Sørg for at modal for analyse kan gjenåpnes
    
    * Bruk mutable swr
    
    * Bruk useswr
    
    * Bruk state fra stilling for analyse
    
    * Legg på flere felt
    
    * Endre styling for analyserstilling
    
    * Putt loader og feilhåndtering inni modal
    
    * Sett på minimumsstørrelse av modal
    
    * Sett på minimumsstørrelse av modal
    
    * Refactor etter innspill
    
    * Bytt til stylet sidelaster
    
    * Bytt til større sidelaster

---
+++ src/api/stillings-api/stillingsanalyse.ts
@@ -0,0 +1,64 @@
+/**
+ * Endepunkt /rekrutteringsbistand/stillingsanalyse
+ */
+import { HttpResponse, http } from 'msw';
+import { z } from 'zod';
+import { postApiWithSchema } from '../fetcher';
+import useSWR from 'swr';
+
+const stillingsanalyseEndepunkt = '/stilling-api/rekrutteringsbistand/stillingsanalyse';
+
+export const stillingsanalyseSchema = z.object({
+    sensitiv: z.boolean(),
+    sensitivBegrunnelse: z.string(),
+    samsvarMedTittel: z.boolean(),
+    tittelBegrunnelse: z.string(),
+    samsvarMedType: z.boolean(),
+    typeBegrunnelse: z.string(),
+});
+
+export const stillingsanalyseRequestSchema = z.object({
+    stillingsId: z.string(),
+    stillingstype: z.string().nullable(),
+    stillingstittel: z.string().nullable(),
+    stillingstekst: z.string(),
+});
+
+export type StillingsanalyseDTO = z.infer<typeof stillingsanalyseSchema>;
+export type StillingsanalyseRequestDTO = z.infer<typeof stillingsanalyseRequestSchema>;
+
+export const useStillingsanalyse = (props: StillingsanalyseRequestDTO, vis: boolean) => {
+    const { stillingsId, stillingstype, stillingstittel, stillingstekst } = props;
+
+    const key =
+        !vis || !stillingstekst || stillingstekst.trim() === ''
+            ? null
+            : [
+                  stillingsanalyseEndepunkt,
+                  stillingsId,
+                  stillingstype,
+                  stillingstittel,
+                  stillingstekst,
+              ];
+
+    const fetcher = () =>
+        postApiWithSchema(stillingsanalyseSchema)({
+            url: stillingsanalyseEndepunkt,
+            body: props,
+        });
+
+    return useSWR(key, fetcher);
+};
+
+export const stillingsanalyseMockMsw = http.post(stillingsanalyseEndepunkt, async (_) => {
+    return HttpResponse.json(mock);
+});
+
+const mock: StillingsanalyseDTO = {
+    sensitiv: true,
+    sensitivBegrunnelse: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    samsvarMedTittel: false,
+    tittelBegrunnelse: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    samsvarMedType: false,
+    typeBegrunnelse: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+};
---
+++ src/stilling/stilling/analyse/AnalyserStillingModal.tsx
@@ -0,0 +1,109 @@
+import { BodyShort, Label, Modal, Alert } from '@navikt/ds-react';
+import css from './AnalyserStillingModal.module.css';
+import Stilling, { hentTittelFraStilling } from 'felles/domene/stilling/Stilling';
+import { useStillingsanalyse } from '../../../api/stillings-api/stillingsanalyse';
+import { useSelector } from 'react-redux';
+import { State } from '../../redux/store';
+import Sidelaster from 'felles/komponenter/sidelaster/Sidelaster';
+
+type IAnalyserStillingModal = {
+    vis: boolean;
+    onClose: () => void;
+    stillingsId: string;
+};
+
+type IAnalyseInnhold = {
+    stillingsId: string;
+    stillingsinfo: any;
+    stillingstittel: string;
+    stilling: Stilling;
+    vis: boolean;
+};
+
+const AnalyseInnhold: React.FC<IAnalyseInnhold> = ({
+    stillingsId,
+    stillingsinfo,
+    stillingstittel,
+    stilling,
+    vis,
+}) => {
+    const { isLoading, data, error } = useStillingsanalyse(
+        {
+            stillingsId: stillingsId || '',
+            stillingstype: stillingsinfo?.stillingskategori || 'Stilling',
+            stillingstittel: stillingstittel || '',
+            stillingstekst: stilling?.properties?.adtext || '',
+        },
+        vis
+    );
+    const stillingsanalyse = data;
+
+    if (isLoading) {
+        return (
+            <div className={css.spinnerContainer}>
+                <Sidelaster size="large" className={css.spinner} />
+            </div>
+        );
+    }
+
+    if (error || stillingsanalyse === undefined) {
+        return (
+            <Alert variant="error" size="small" fullWidth>
+                Klarte ikke å analysere stillingen.
+            </Alert>
+        );
+    }
+
+    return (
+        <div className={css.innhold}>
+            <div>
+                <Label>Sensitiv</Label>
+                <BodyShort>{stillingsanalyse.sensitiv ? 'Ja' : 'Nei'}</BodyShort>
+                <BodyShort>{stillingsanalyse.sensitivBegrunnelse}</BodyShort>
+            </div>
+            <div>
+                <Label>Samsvar med type</Label>
+                <BodyShort>{stillingsanalyse.samsvarMedType ? 'Ja' : 'Nei'}</BodyShort>
+                <BodyShort>{stillingsanalyse.typeBegrunnelse}</BodyShort>
+            </div>
+            <div>
+                <Label>Samsvar med tittel</Label>
+                <BodyShort>{stillingsanalyse.samsvarMedTittel ? 'Ja' : 'Nei'}</BodyShort>
+                <BodyShort>{stillingsanalyse.tittelBegrunnelse}</BodyShort>
+            </div>
+        </div>
+    );
+};
+
+const AnalyserStillingModal: React.FC<IAnalyserStillingModal> = ({ vis, onClose, stillingsId }) => {
+    // Henter stillingen basert på stillingsId
+    const stilling = useSelector((state: State) => state.adData) as Stilling;
+    const stillingsinfo = useSelector((state: State) => state.stillingsinfoData);
+
+    const stillingstittel = hentTittelFraStilling(stilling);
+
+    return (
+        <Modal
+            open={vis}
+            aria-label="Analyser stilling"
+            onClose={onClose}
+            header={{
+                heading: 'Analyser stilling',
+            }}
+        >
+            <Modal.Body>
+                <div className={css.analyserstilling}>
+                    <AnalyseInnhold
+                        stillingsId={stillingsId}
+                        stillingsinfo={stillingsinfo}
+                        stillingstittel={stillingstittel}
+                        stilling={stilling}
+                        vis={vis}
+                    />
+                </div>
+            </Modal.Body>
+        </Modal>
+    );
+};
+
+export default AnalyserStillingModal;
