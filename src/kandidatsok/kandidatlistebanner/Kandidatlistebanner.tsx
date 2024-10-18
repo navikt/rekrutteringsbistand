6c73dd4e67cd70ab70e8c6da526645dbdf6d22eb
Erlend Wiklem <erlend.wiklem@nav.no>
2024-10-18 11:25:59 +0200

    Finn kandidater for stilling

--- src/kandidatsok/kandidatlistebanner/Kandidatlistebanner.tsx
+++ src/kandidatsok/kandidatlistebanner/Kandidatlistebanner.tsx
@@ -7,15 +7,35 @@ import { FunctionComponent } from 'react';
 import { Link } from 'react-router-dom';
 import useHentStilling, { useHentStillingTittel } from '../../felles/hooks/useStilling';
 import { lenkeTilStilling } from '../../felles/lenker';
+import useNavigeringsstate from '../hooks/useNavigeringsstate';
 import useSøkekriterierFraStilling from '../hooks/useSøkekriterierFraStilling';
 import css from './Kandidatlistebanner.module.css';
-import useNavigeringsstate from '../hooks/useNavigeringsstate';
 
 type Props = {
     stillingId?: string;
 };
 
 const Kandidatlistebanner: FunctionComponent<Props> = ({ stillingId }) => {
+    if (stillingId) {
+        return <KandidatlistebannerVisningStilling stillingId={stillingId} />;
+    }
+
+    return <KandidatlistebannerVisning />;
+};
+
+const KandidatlistebannerVisningStilling = ({ stillingId }: { stillingId: string }) => {
+    const { stilling, isLoading } = useHentStilling(stillingId);
+    const navigeringsstate = useNavigeringsstate();
+    const brukKriterierFraStillingen = navigeringsstate.brukKriterierFraStillingen;
+    useSøkekriterierFraStilling(stilling, brukKriterierFraStillingen);
+    if (isLoading) {
+        return <Loader size="medium" />;
+    }
+
+    return <KandidatlistebannerVisning stillingId={stillingId} />;
+};
+
+const KandidatlistebannerVisning: FunctionComponent<Props> = ({ stillingId }) => {
     const navigeringsstate = useNavigeringsstate();
     const {
         stilling: rekrutteringsbistandstilling,
