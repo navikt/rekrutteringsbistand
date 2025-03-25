9b48bc17ec1648e95390a3e17f0ab9371e9a6cbf
Joar Aurdal <13847640+joarau@users.noreply.github.com>
2025-03-25 13:39:30 +0100

    Endr tekst for alert for organisasjonsnummerendring

--- src/kandidat/kandidatliste/OrganisasjonsnummerAlert.tsx
+++ src/kandidat/kandidatliste/OrganisasjonsnummerAlert.tsx
@@ -1,63 +1,69 @@
-import { Alert, BodyLong, Box, Heading, Link, List } from '@navikt/ds-react';
+import { Alert, BodyLong, Heading, Link, List, VStack } from '@navikt/ds-react';
 import css from './OrganisasjonsnummerAlert.module.css';
 
 const OrganisasjonsnummerAlert = () => (
-    <div className={css.organisasjonsnummeralert}>
-        <Alert variant="warning">
-            <Heading level="3" size="medium" spacing>
-                Stillingen har fått nytt organisasjonsnummer
-            </Heading>
-            <BodyLong className={css.beskrivelse}>
-                Organisasjonsnummeret til stillingen har blitt endret i et annet system. Det kan
-                føre til at CVer deles med feil organisasjon.
-            </BodyLong>
+    <Alert variant="warning" className={css.organisasjonsnummeralert}>
+        <VStack gap="4">
+            <div>
+                <Heading level="3" size="medium" spacing>
+                    Stillingen har fått nytt organisasjonsnummer
+                </Heading>
+                <BodyLong>
+                    Organisasjonsnummeret til stillingen har blitt endret i et annet system, men
+                    ikke i kandidatlisten. Det kan føre til at CVer deles med feil organisasjon,
+                    siden det ikke er mulig å endre organisasjonsnummer på kandidatlisten.
+                </BodyLong>
+            </div>
 
-            <Box paddingBlock="4">
+            <div>
                 <Heading level="4" size="small">
                     Hva betyr det for meg?
                 </Heading>
                 <BodyLong>
-                    Har du lagt til kandidater på stillingen blir de delt med den forrige
-                    organisasjonen. Deler du CVene deres, vil det føre til personvernsbrudd.
+                    Legger du til kandidater på stillingen og deler CV med arbeidsgiver blir de delt
+                    med den forrige organisasjonen. Deler du CVene deres, vil det føre til
+                    personvernsbrudd.
                 </BodyLong>
-            </Box>
+            </div>
 
-            <List
-                as="ol"
-                title="Hva kan jeg gjøre med det?"
-                description="For å dele CVene til disse kandidatene må du:"
-                headingTag="h4"
-            >
-                <List.Item>Slett kandidatene fra denne stillingen.</List.Item>
-                <List.Item>
-                    Opprette stillingen på nytt med det nye organisasjonsnummeret.
-                </List.Item>
-                <List.Item>Legge til kandidatene igjen på den nye stillingen.</List.Item>
-            </List>
+            <div>
+                <Heading level="4" size="small">
+                    Hva kan jeg gjøre med det?
+                </Heading>
+                <BodyLong>For å dele CVene til disse kandidatene må du:</BodyLong>
+                <List as="ol" className="mt-4">
+                    <List.Item>Slett kandidatene fra denne stillingen.</List.Item>
+                    <List.Item>
+                        Opprette en direktemeldt stilling med det nye organisasjonsnummeret
+                    </List.Item>
+                    <List.Item>Legge til kandidatene igjen på den nye stillingen.</List.Item>
+                </List>
+            </div>
 
-            <List
-                as="ol"
-                title="Hva hvis jeg har delt CVene til disse kandidatene allerede?"
-                headingTag="h4"
-            >
-                <List.Item>
-                    Slett delingen: Finn kandidatene i listen, trykk på redigeringsknappen, og velg
-                    "Slett CV hos arbeidsgiver".
-                </List.Item>
-                <List.Item>
-                    Meld avvik i{' '}
-                    <Link
-                        target="_blank"
-                        rel="noreferrer noopener"
-                        href="https://navno.sharepoint.com/sites/intranett-avvik"
-                    >
-                        avviksrapporteringssystemet
-                    </Link>{' '}
-                    med informasjon om hva som skjedde.
-                </List.Item>
-            </List>
-        </Alert>
-    </div>
+            <div>
+                <Heading level="4" size="small">
+                    Hva hvis jeg har delt CVene til disse kandidatene allerede?
+                </Heading>
+                <List as="ol" className="mt-4">
+                    <List.Item>
+                        Slett delingen: Finn kandidatene i listen, trykk på redigeringsknappen, og
+                        velg &quot;Slett CV hos arbeidsgiver&quot;.
+                    </List.Item>
+                    <List.Item>
+                        Meld avvik i{' '}
+                        <Link
+                            target="_blank"
+                            rel="noreferrer noopener"
+                            href="https://navno.sharepoint.com/sites/intranett-avvik"
+                        >
+                            avviksrapporteringssystemet
+                        </Link>{' '}
+                        med informasjon om hva som skjedde.
+                    </List.Item>
+                </List>
+            </div>
+        </VStack>
+    </Alert>
 );
 
 export default OrganisasjonsnummerAlert;
